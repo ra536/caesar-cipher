ad8957316eae4c3e28dbfd7aff707d869d874a1c
Robert Argasinski <ra536@njit.edu>
2021-09-21 16:43:44 -0400

    Code structure added

--- caesar.s
+++ caesar.s
@@ -3,19 +3,50 @@
         HelloWorld:
                 .asciz "Hello World!\n"
 		lenHelloWorld = .-HelloWorld
+
+	# plaintext prompt
+	# plaintext prompt length
+
+	# shift key prompt
+	# shift key prompt length
+
 .text
 
         .globl _start
+	.type CaesarCipher, @function
+
+	CaesarCipher:
+		# find plaintext on stack
+		# find shift key on stack
+
+		# convert shift key to integer
+
+		# LOOP (calculate ciphertext)
+
+		# print ciphertext
+
+		ret
 
         _start:
+		# read system call for plaintext
+		# push plaintext to stack
 
+		# read system call for shift key
+		# push shift key to stack
+
+		# call CaesarCipher
+
+		# adjust stack pointer	
+
+	
+		# Hello World Example
                 movl $4, %eax
                 movl $1, %ebx
                 movl $HelloWorld, %ecx
                 movl $lenHelloWorld, %edx
                 int $0x80
 
-
+		# Quit
                 movl $1, %eax
                 movl $0, %ebx
                 int $0x80
