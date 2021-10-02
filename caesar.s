4506212ce7a4d8f7a122962c482a9b71dc51aefc
Nishaant Goswamy <33434267+Nishaant215@users.noreply.github.com>
2021-10-02 13:13:00 -0400

    Fixed some comments

--- caesar.s
+++ caesar.s
@@ -2,29 +2,29 @@
 
 	
 	PlaintextPrompt:
-		.asciz "Please enter the plaintext: "        # plaintext prompt
+		.asciz "Please enter the plaintext: "        # plaintext prompt string
 		lenPlaintextPrompt = .-PlaintextPrompt	     # plaintext prompt length
 
 	
 	ShiftKeyPrompt:
-		.asciz "Please enter the shift value: "	     # shift key prompt
-		lenShiftKeyPrompt = .-ShiftKeyPrompt	     # shift key prompt length
+		.asciz "Please enter the shift value: "	     # shift key prompt string
+		lenShiftKeyPrompt = .-ShiftKeyPrompt	     # shift key prompt string length
 	
-	PowerOfTen:					     # This PowerOfTen var will be used to upscale the digits from ones to tens or hundreds group
+	PowerOfTen:					     # This PowerOfTen var will be used to upscale the digits from ones to tens to hundreds group
 		.int 0x1   				    
 
-	Conversion:					    # This Conversion var will hold the ASCII translated hexadecimal number
+	Conversion:					    # This Conversion var will hold the translated ASCII number in hexadecimal format
 		.int 0x0		
 
 .bss	
 	.comm Plaintext, 51				    # allocates 51 bytes for plaintext which is 50 characters + 1 null byte
-	.comm PlaintextLength, 4			    # allocates 4 bytes for  plaintext length value of 51
+	.comm PlaintextLength, 4			    # allocates 4 bytes for plaintext length value of 51
 
-	.comm ShiftKey, 4				    # allocates 4 bytes for shift key number which is inputted in ASCII.
-	.comm ShiftKeyLength, 4				    # allocates 4 bytes for shift key's length value. 
+	.comm ShiftKey, 4				    # allocates 4 bytes for shift key number which is inputted in ASCII format.
+	.comm ShiftKeyLength, 4				    # allocates 4 bytes for shift key's length value up 1000 
 
-	.comm Ciphertext, 51				   # allocates 51 bytes for cipher output which is 50 characters + 1 null byte
-	.comm CiphertextLength, 4			   # allocates 4 ytes for  cipher output's length value.
+	.comm Ciphertext, 51				   # allocates 51 bytes for cipher output which is 50 characters + 1 null byte (ASCII format)
+	.comm CiphertextLength, 4			   # allocates 4 ytes for cipher output's length value of 51
 
 .text
 
@@ -32,11 +32,8 @@
 	.type CaesarCipher, @function
 
 	CaesarCipher:
-
-		pointers:
-			
-                	pushl %ebp				# Store value of EBP on stack
-				
+		pointers:			
+                	pushl %ebp				# Store value of EBP on stack				
                 	movl %esp, %ebp				# Make EBP point to top of stack
 
 		setup:
@@ -46,43 +43,43 @@
 		
 		modConversion:
 			cmp $26, %ebx 				# check if the shiftKey number is less than 26. 
-			jb shiftLoop 				# if the number is less than 26 then we do the shift 
-		
+			jb shiftLoop 				# if the shiftKey number is less than 26 then we found the modulo 26 of the ShiftKey number 
+								# now we can start the shift operation
 		subConversion:
-			sub $26, %ebx				# if the number is larger than 26 we repeately subtract by 26 to find the modules of 26
-			jmp modConversion
+			sub $26, %ebx				# if the shiftKey number is larger than 26 we repeately subtract 26 
+			jmp modConversion			# and compare the shiftKey number until we find modulo 26 of the ShiftKey number
 
 		shiftLoop:
 			movl $0x0, %eax				# reset %eax to zero 
-			lodsb					# loads character byte from %esi(PlaintextPointer) into %al register to do shift operation	
-			cmp $0x0a, %al				# compare byte in %al register with newline character to see if were at the end of line
+			lodsb					# loads character byte from %esi(PlaintextPointer) into %al register to start shift operation	
+			
+			cmp $0x0a, %al				# compares byte in %al register with newline character to see if at the end of line
 			jz return				# if we are at the end of line then we finish shifting
 			
-			cmp $0x20, %al		               # compare with space char 
-			jz store			       # if it space char then store the space char do not shift it
+			cmp $0x20, %al		               # compares with space char 
+			jz store			       # if it space char then store the space char and do not shift it
 			
 			#SHIFT
-			sub $65, %al                          # subtract 65 from the %al register to scale down ASCII letter to 0-25 range
-			add %bl, %al			      # add the shift value to plaintext letter
-
+			sub $65, %al                          # else, subtract 65 from the %al register to scale down ASCII letter to alphabet (0-25) range
+			add %bl, %al			      # add the shiftkey value to plaintext letter to shift the letter
 
 		modPlaintext:
-			cmp $26, %al			       #compare that the added value is within the bounds of the alphabet (0-25)
-			jb donePlaintext 		       # if is wihin bounds jump to donePlainText to translate back to ASCII
+			cmp $26, %al			       # compare to check if the newly shifted value is within the bounds of the alphabet (0-25) range
+			jb donePlaintext 		       # if is within bounds jump to donePlainText to translate back to ASCII format
 		
 		subPlaintext:
-			sub $26, %al			      # substract 26 if the added value is greater then bounds of the alphabet values (0-25)
+			sub $26, %al			      # substract 26 if the newly shifted value is greater then bounds of the alphabet values (0-25)
 			jmp modPlaintext		      # jump to modPlaintext to compare again
 
 		donePlaintext:
 			add $65, %al			      # translates the value back to ASCII format by adding 65 
 
 		store:
-			stosb				 # stores the letter from %al register into memory %edi (Ciphertext memory location) 
+			stosb				 # stores the ASCII letter from %al register into memory %edi (Ciphertext memory location) 
 			jmp shiftLoop    		 # restart the loop for the next letter in word
 
 		return:
-			mov $0x0a, %al			#adds a newline character and stores in the %edi (Ciphertext memory location)
+			mov $0x0a, %al			# moves a newline character in al% to later store it in the %edi (Ciphertext memory location)
 			stosb
 
 			movl %ebp, %esp         # Restore the old value of ESP
@@ -92,28 +89,24 @@
 
 	.type StringShiftKeytoInt, @function
 
-	StringShiftKeytoInt: 
-	
+	StringShiftKeytoInt: 	
 		StackSetup: 
 		       push %ebp 			  # pushes %ebp on the stack
-
-		       movl %esp, %ebp			  # makes %ebp to the new pointer to top of the stack
-                       
+		       movl %esp, %ebp			  # Make EBP point to top of stack                     
 		       movl 8(%ebp), %esi    	          # Shiftkey's value in ASCII is stored in %esi
-
-	               movl $0x0, %ecx			  # set up counter an prep %esi for lodsb command
+	               movl $0x0, %ecx			  # set up digit counter for ShiftKey value
 	                      
 	   countShiftKeyDigits:
-			lodsb				# load next byte into %al to be converted in hexadecimal value
+			lodsb				# load next byte into %al register from %esi to parse through the Shiftkey's value in ASCII digit by digit
 							
-			cmp $0x0a, %al			# if reached to newline, finish execution and stop the counting the digits
+			cmp $0x0a, %al			# if reached to newline, finish execution and stop counting the digits
 			jz locateLastDigit
 							
 			inc %ecx			# else, increment the digits counter and repeat loop	
 			jmp countShiftKeyDigits 
 					
 		locateLastDigit:
-           	        std  				 # changes direction to read digits from lowest order first
+           	        std  				# changes direction to read digits from lowest order first
 
 			lodsb  				#skip over null character since the last lodsb left pointer at a null character
 		        lodsb  				#skip over new line
@@ -121,7 +114,7 @@
 	        convertInt:
 			movl $0x0, %eax   	       # clear out %eax, since lodsb only fills lowest byte
 			
-			lodsb            	       # load next byte into %al from %esi
+			lodsb            	       # load digits from lowest order first into %al from %esi (Shiftkey value)
 			
                         dec %ecx        	       # decrement counter
 
@@ -129,17 +122,15 @@
 
 			imul PowerOfTen, %eax           # scale up the digit, depending on place value in hex
 			
-			addl %eax, Conversion           # adds on the next hundreds,tens, ones group
+			addl %eax, Conversion           # adds on the next ones,tens, hundreds group
 
-			imul $10, PowerOfTen, %ebx      # multiply PowerOfTen by factor of 10, save to PowerOfTen label
+			imul $10, PowerOfTen, %ebx      # multiplies PowerOfTen by factor of 10, and saves to PowerOfTen
 		        movl %ebx, PowerOfTen	      
 
-			cmp $0x0, %ecx                    # if all digits read, continue looping for the next digit
+			cmp $0x0, %ecx                  # if all digits read stop converting, else continue looping for the next digit
 			jnz convertInt
 		
-		
 			cld				# Clear Flags
-
 		return2:
 			movl %ebp, %esp         # Restore the old value of ESP
 			popl %ebp               # Restore the old value of EBP
@@ -180,7 +171,7 @@
 		
 	callStringShiftKeytoInt:
 		pushl $ShiftKey                      # passes the ShfitKey value to the stack
-	
+		
 		call StringShiftKeytoInt  	     # calls the function to convert the string ShiftKey value to an hexadecimal value
 
 		addl $4, %esp			     # adjust the stack pointer
@@ -199,7 +190,7 @@
 		movl $4, %eax			   # syscall for write() 
 		movl $1, %ebx			   # File descriptor for std_out
 		movl $Ciphertext, %ecx		   # moves the address of the CiphertextPointer string into %ecx
-		movl PlaintextLength, %edx         # moves the length of CiphertextLength var into %edx
+		movl PlaintextLength, %edx         # moves the length of PlaintextLength into %edx
 		int $0x80			    # calls kernel		
 
 		# Quit
