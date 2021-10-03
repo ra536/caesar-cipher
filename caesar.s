1300c8542fa71ed07afae969e150b091db30f85d
Robert Argasinski <ra536@njit.edu>
2021-10-03 14:55:31 -0400

    Finish modifying comments

--- caesar.s
+++ caesar.s
@@ -86,7 +86,7 @@
 
 			movl %ebp, %esp         		# restore the old value of ESP
                 	popl %ebp              			# restore the old value of EBP from the stack
-			ret
+			ret					# return from function
 
 
 	.type StringShiftKeytoInt, @function
@@ -95,107 +95,107 @@
 		StackSetup: 
 		       push %ebp 			  # save existing value of EBP on the stack
 		       movl %esp, %ebp			  # Make EBP point to the top of the stack                     
-		       movl 8(%ebp), %esi    	          # Shiftkey's value in ASCII is stored in %esi
+		       movl 8(%ebp), %esi    	          # load ShiftKey string into ESI
 	               movl $0x0, %ecx			  # set up digit counter for ShiftKey value
 	                      
 	   countShiftKeyDigits:
-			lodsb				# load next byte into %al register from %esi to parse through the Shiftkey's value in ASCII digit by digit
+			lodsb				# load next byte of the ShiftKey into AL
 							
-			cmp $0x0a, %al			# if reached to newline, finish execution and stop counting the digits
-			jz locateLastDigit
+			cmp $0x0a, %al			# if reached to newline, 
+			jz locateLastDigit		# finish counting and jump to conversion step
 							
-			inc %ecx			# else, increment the digits counter and repeat loop	
-			jmp countShiftKeyDigits 
+			inc %ecx			# else, increment the counter
+			jmp countShiftKeyDigits		# and repeat the loop	 
 					
 		locateLastDigit:
-           	        std  				# changes direction to read digits from lowest order first
+           	        std  				# sets direction flag to read digits from lowest order byte first
 
-			lodsb  				#skip over null character since the last lodsb left pointer at a null character
-		        lodsb  				#skip over new line
+			lodsb  				# skip over null character, since the last lodsb left pointer at a null character
+		        lodsb  				# skip over newline character
 
 	        convertInt:
-			movl $0x0, %eax   	       # clear out %eax, since lodsb only fills lowest byte
+			movl $0x0, %eax   	        # clear out EAX, since lodsb only fills lowest order byte
 			
-			lodsb            	       # load digits from lowest order first into %al from %esi (Shiftkey value)
+			lodsb            	        # load next ASCII character into AL 
 			
-                        dec %ecx        	       # decrement counter
+                        dec %ecx        	        # decrement counter
 
-			sub $0x30,%eax         		# convert ASCII character to corresponding integer in hex
+			sub $0x30, %eax        		# convert ASCII character to corresponding integer
 
-			imul PowerOfTen, %eax           # scale up the digit, depending on place value in hex
+			imul PowerOfTen, %eax           # scale up the digit, depending on place value (initially 1, for one's place) 
 			
-			addl %eax, Conversion           # adds on the next ones,tens, hundreds group
+			addl %eax, Conversion           # add value to running Conversion total
 
-			imul $10, PowerOfTen, %ebx      # multiplies PowerOfTen by factor of 10, and saves to PowerOfTen
-		        movl %ebx, PowerOfTen	      
+			imul $10, PowerOfTen, %ebx      # multiplies PowerOfTen by factor of 10, 
+		        movl %ebx, PowerOfTen	        # and saves to PowerOfTen 
 
-			cmp $0x0, %ecx                  # if all digits read stop converting, else continue looping for the next digit
-			jnz convertInt
+			cmp $0x0, %ecx                  # if all digits read stop converting (fall through)
+			jnz convertInt			# else, continue looping for the next digit
 		
-			cld				# Clear Flags
+			cld				# clear direction flag
+
 		return2:
-			movl %ebp, %esp         # Restore the old value of ESP
-			popl %ebp               # Restore the old value of EBP
-			ret
+			movl %ebp, %esp         # restore the old value of ESP
+			popl %ebp               # restore the old value of EBP
+			ret			# return from function
 
         _start:
-		# write system call 
+		# write system call for plaintext prompt 
 		movl $4, %eax			     # syscall for write()
-		movl $1, %ebx			     # File descriptor for std_out 
-		movl $PlaintextPrompt, %ecx          # moves the address of the Plaintext Prompt string into ecx
-		movl $lenPlaintextPrompt, %edx	     # moves the length of the Plaintext Prompt string into edx
-		int $0x80			     # calls kernel
+		movl $1, %ebx			     # file descriptor for std_out 
+		movl $PlaintextPrompt, %ecx          # load the PlaintextPrompt string
+		movl $lenPlaintextPrompt, %edx	     # load length of the PlaintextPrompt string
+		int $0x80			     # calls kernel interrupt
 
 		# read system call for plaintext
 		movl $3, %eax			    # syscall for read()
-		movl $0x0, %ebx			    # File descriptor for std_in  
-		movl $Plaintext, %ecx		    # moves the address of the PlaintextPointer var into ecx
-		movl $51, %edx			    # moves the length of Plaintext var into edx 
-		int $0x80			    # calls kernel
+		movl $0x0, %ebx			    # file descriptor for std_in  
+		movl $Plaintext, %ecx		    # load the Plaintext
+		movl $52, %edx			    # load 52 for plaintext length (PlaintextLength CANNOT be used, since uninitialized) 
+		int $0x80			    # calls kernel interrupt
 		
-		movl %eax, PlaintextLength          # total length of input (input + newline)
+		movl %eax, PlaintextLength          # read call returns total length of input (user input + newline)
 
-		# write system call 		    
+		# write system call for shift key prompt 		    
 		movl $4, %eax			    # syscall for write() 
-		movl $1, %ebx			    # File descriptor for std_out
-		movl $ShiftKeyPrompt, %ecx          # moves the address of the ShiftKeyPrompt string into ecx
-		movl $lenShiftKeyPrompt, %edx       # moves the length of the ShiftKeyPrompt string into edx
-		int $0x80			    # calls kernel 
+		movl $1, %ebx			    # file descriptor for std_out
+		movl $ShiftKeyPrompt, %ecx          # load the ShiftKeyPrompt string 
+		movl $lenShiftKeyPrompt, %edx       # load length of the ShiftKeyPrompt string 
+		int $0x80			    # calls kernel interrupt 
 
 		# read system call for shift key
 		movl $3, %eax			    # syscall for read()
-		movl $0x0,  %ebx		    # File descriptor for std_in 
-		movl $ShiftKey, %ecx		    # moves the address of the ShiftKeyPointer var into ecx         
-		movl $4, %edx			    # moves the length of the ShiftKeyPointer var into edx
-		int $0x80			    # calls kernel
+		movl $0x0,  %ebx		    # file descriptor for std_in 
+		movl $ShiftKey, %ecx		    # load the ShiftKey
+		movl $5, %edx			    # load 5 for shift key length (ShiftKeyLength CANNOT be used, since uninitialized)
+		int $0x80			    # calls kernel interrupt
 
-		movl %eax, ShiftKeyLength           # total length of input (input + newline)
+		movl %eax, ShiftKeyLength           # read call returns total length of input (user input + newline)
 		
 	callStringShiftKeytoInt:
-		pushl $ShiftKey                      # passes the ShfitKey value to the stack
+		pushl $ShiftKey                      # saves ShiftKey to the stack
 		
-		call StringShiftKeytoInt  	     # calls the function to convert the string ShiftKey value to an hexadecimal value
+		call StringShiftKeytoInt  	     # calls function to convert the ShiftKey string value to an integer value
 
-		addl $4, %esp			     # adjust the stack pointer, since $ShiftKey was pushed to stack
+		addl $4, %esp			     # adjust the stack pointer, since ShiftKey was pushed to the stack
 		
 	callCaesarCipher:
-                pushl $Plaintext		     # Pushing Plaintext to stack
-               
-                pushl Conversion 	             # Pushing Conversion to stack
+                pushl $Plaintext		     # pushing Plaintext to stack
+                pushl Conversion 	             # pushing Conversion to stack
 
-		call CaesarCipher	             # Call the Caesar Cipher funtion
+		call CaesarCipher	             # call function to perform caesar cipher
 
-                addl $8, %esp			    # adjust the stack pointer, since $Plaintext and Conversion was pushed to stack
+                addl $8, %esp			     # adjust the stack pointer, since Plaintext and Conversion were pushed to the stack
 
 	finish:
-		# write system call 
+		# write system call for ciphertext 
 		movl $4, %eax			   # syscall for write() 
-		movl $1, %ebx			   # File descriptor for std_out
-		movl $Ciphertext, %ecx		   # moves the address of the CiphertextPointer string into %ecx
-		movl PlaintextLength, %edx         # moves the length of PlaintextLength into %edx
-		int $0x80			    # calls kernel		
+		movl $1, %ebx			   # file descriptor for std_out
+		movl $Ciphertext, %ecx		   # load Ciphertext 
+		movl PlaintextLength, %edx         # load PlaintextLength (since plaintext is the same size as ciphertext)
+		int $0x80			   # calls kernel interrupt		
 
 		# Quit
                 movl $1, %eax			# sys_exit
-                movl $0, %ebx
-                int $0x80			# calls kernel	
+                movl $0, %ebx			# return value of 0
+                int $0x80			# calls kernel interrupt	
